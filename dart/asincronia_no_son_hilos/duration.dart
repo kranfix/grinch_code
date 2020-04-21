333b9afa961fcf6e834d151d17fb258b8f815271
Frank Moreno <frankmoreno1993@gmail.com>
2020-04-21 17:02:12 -0500

    Ejemplo de duration

---
+++ dart/asincronia_no_son_hilos/duration.dart
@@ -0,0 +1,13 @@
+void main() {
+  final duration1 = Duration(
+    hours: 2,
+    seconds: 1,
+  );
+
+  final duration2 = Duration(
+    hours: 1,
+    minutes: 20,
+  );
+
+  print(duration1 - duration2);
+}
