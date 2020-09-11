9a38f22075dc1b74ae7476830e1c74683f7f8084
Frank Moreno <frankmoreno1993@gmail.com>
2020-09-11 17:10:11 -0500

    TodoRepo.findAll

--- flutter_riverpod_todolist/lib/todo_repo.dart
+++ flutter_riverpod_todolist/lib/todo_repo.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import './todo_list_provider.dart';
+import './todo.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:http/http.dart' as http;
 
 final todoRepoProvider = Provider((ref) => TodoRepo(ref.read));
 
@@ -8,4 +12,15 @@ class TodoRepo {
   const TodoRepo(this.read) : assert(read != null, 'ref must be non-null');
 
   final Reader read;
+
+  Future<void> findAll() async {
+    final response = await http.get('$_url?query={todoList{id,text,done}}');
+    final result = json.decode(response.body) as Map<String, dynamic>;
+    if (result['errors'] != null) {
+      print(result['errors']);
+      throw Exception('Error reading all todos');
+    }
+    final todoList = read(todoListProvider);
+    todoList.addAll(Todo.parseList(result['data']['todoList']));
+  }
 }
