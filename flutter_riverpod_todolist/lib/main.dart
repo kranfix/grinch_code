2ae568ff87be5602e20639fa1e40650a5cadc509
Frank Moreno <frankmoreno1993@gmail.com>
2020-09-11 18:20:27 -0500

    flutter TodoList example main

---
+++ flutter_riverpod_todolist/lib/main.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'todo.dart';
+import 'todo_list_provider.dart';
+import 'todo_repo.dart';
+
+/// Some keys used for testing
+final addTodoKey = UniqueKey();
+final activeFilterKey = UniqueKey();
+final completedFilterKey = UniqueKey();
+final allFilterKey = UniqueKey();
+
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends HookWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final repo = useProvider(todoRepoProvider);
+    useMemoized(() {
+      repo.findAll();
+    });
+    final todos = useProvider(filteredTodos);
+    final newTodoController = useTextEditingController();
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          children: [
+            const Title(),
+            TextField(
+              key: addTodoKey,
+              controller: newTodoController,
+              decoration: const InputDecoration(
+                labelText: 'What needs to be done?',
+              ),
+              onSubmitted: (value) async {
+                try {
+                  await context.read(todoRepoProvider).createOne(value);
+                  newTodoController.clear();
+                } catch (e) {}
+              },
+            ),
+            const SizedBox(height: 42),
+            const Toolbar(),
+            if (todos.isNotEmpty) const Divider(height: 0),
+            for (var i = 0; i < todos.length; i++) ...[
+              if (i > 0) const Divider(height: 0),
+              Dismissible(
+                key: ValueKey(todos[i].id),
+                onDismissed: (_) {
+                  context.read(todoListProvider).remove(todos[i]);
+                },
+                child: ProviderScope(
+                  overrides: [
+                    _currentTodo.overrideWithValue(todos[i]),
+                  ],
+                  child: const TodoItem(),
+                ),
+              )
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Toolbar extends HookWidget {
+  const Toolbar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final filter = useProvider(todoListFilter);
+
+    Color textColorFor(TodoListFilter value) {
+      return filter.state == value ? Colors.blue : null;
+    }
+
+    return Material(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text(
+              '${useProvider(uncompletedTodosCount).toString()} items left',
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Tooltip(
+            key: allFilterKey,
+            message: 'All todos',
+            child: FlatButton(
+              onPressed: () => filter.state = TodoListFilter.all,
+              visualDensity: VisualDensity.compact,
+              textColor: textColorFor(TodoListFilter.all),
+              child: const Text('All'),
+            ),
+          ),
+          Tooltip(
+            key: activeFilterKey,
+            message: 'Only uncompleted todos',
+            child: FlatButton(
+              onPressed: () => filter.state = TodoListFilter.active,
+              visualDensity: VisualDensity.compact,
+              textColor: textColorFor(TodoListFilter.active),
+              child: const Text('Active'),
+            ),
+          ),
+          Tooltip(
+            key: completedFilterKey,
+            message: 'Only completed todos',
+            child: FlatButton(
+              onPressed: () => filter.state = TodoListFilter.completed,
+              visualDensity: VisualDensity.compact,
+              textColor: textColorFor(TodoListFilter.completed),
+              child: const Text('Completed'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'todos',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Color.fromARGB(38, 47, 47, 247),
+        fontSize: 100,
+        fontWeight: FontWeight.w100,
+        fontFamily: 'Helvetica Neue',
+      ),
+    );
+  }
+}
+
+/// A provider which exposes the [Todo] displayed by a [TodoItem].
+///
+/// By retreiving the [Todo] through a provider instead of through its
+/// constructor, this allows [TodoItem] to be instantiated using the `const` keyword.
+///
+/// This ensures that when we add/remove/edit todos, only what the
+/// impacted widgets rebuilds, instead of the entire list of items.
+final _currentTodo = ScopedProvider<Todo>(null);
+
+class TodoItem extends HookWidget {
+  const TodoItem({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final todo = useProvider(_currentTodo);
+    final itemFocusNode = useFocusNode();
+    // listen to focus chances
+    useListenable(itemFocusNode);
+    final isFocused = itemFocusNode.hasFocus;
+
+    final textEditingController = useTextEditingController();
+    final textFieldFocusNode = useFocusNode();
+
+    return Material(
+      color: Colors.white,
+      elevation: 6,
+      child: Focus(
+        focusNode: itemFocusNode,
+        onFocusChange: (focused) {
+          if (focused) {
+            textEditingController.text = todo.text;
+          } else {
+            // Commit changes only when the textfield is unfocused, for performance
+            context
+                .read(todoRepoProvider)
+                .editOne(todo, textEditingController.text);
+          }
+        },
+        child: ListTile(
+          onTap: () {
+            itemFocusNode.requestFocus();
+            textFieldFocusNode.requestFocus();
+          },
+          leading: Checkbox(
+            value: todo.done,
+            onChanged: (value) =>
+                context.read(todoRepoProvider).toggleOne(todo),
+          ),
+          title: isFocused
+              ? TextField(
+                  autofocus: true,
+                  focusNode: textFieldFocusNode,
+                  controller: textEditingController,
+                )
+              : Text(todo.text),
+        ),
+      ),
+    );
+  }
+}
